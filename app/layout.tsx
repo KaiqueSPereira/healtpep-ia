f57d924c3e9ec7effac909e2d746b3ee338abfb6
kaiquekdsp <kaiquekdsp@gmail.com>
2025-07-13 19:56:03 +0000

    Atualizado o layout do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata = {
 
   title: "Health Pep",
   description: "Seu prontuario medico pessoal",
+  icons: {
+    icon: "/iconprontuario.png",
+  },
 };
 
 export default function RootLayout({
