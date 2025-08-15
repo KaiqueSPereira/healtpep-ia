aef6dd0ee87e1756e4a2d533c50293d2c8baf661
kaiquekdsp <kaiquekdsp@gmail.com>
2025-08-15 14:35:00 +0000

    Atualizado o filtro de profissional na tela de consulta para mostrar apenas o profissional cadastrado pelo usuário logado na tela de consulta.

--- app/consulta/page.tsx
+++ app/consulta/page.tsx
@@ -11,50 +11,50 @@ import ConsultaFilter from './components/ConsultaFilter';
 
 
 const parseDate = (dateString: string) => {
-   const regexFullDate = /^(\d{2})\/(\d{2})\/(\d{4})$/;
-   const regexYearShort = /^(\d{2})\/(\d{2})\/(\d{2})$/;
-   const regexMonthDay = /^(\d{2})\/(\d{2})$/;
-   const regexDayOnly = /^(\d{2})$/;
+  const regexFullDate = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+  const regexYearShort = /^(\d{2})\/(\d{2})\/(\d{2})$/;
+  const regexMonthDay = /^(\d{2})\/(\d{2})$/;
+  const regexDayOnly = /^(\d{2})$/;
 
-   let date: Date | null = null;
+  let date: Date | null = null;
 
-   const matchFullDate = dateString.match(regexFullDate);
-   if (matchFullDate) {
-     const [, day, month, year] = matchFullDate;
-     date = new Date(`${year}-${month}-${day}`);
-   } else {
-     const matchShortYear = dateString.match(regexYearShort);
-     if (matchShortYear) {
-       const [, day, month, year] = matchShortYear;
-       date = new Date(`20${year}-${month}-${day}`);
-     } else {
-       const matchMonthDay = dateString.match(regexMonthDay);
-       if (matchMonthDay) {
-         const [, day, month] = matchMonthDay;
-         date = new Date(
-           new Date().getFullYear(),
-           parseInt(month) - 1,
-           parseInt(day),
-         );
-       } else {
-         const matchDayOnly = dateString.match(regexDayOnly);
-         if (matchDayOnly) {
-           const [, day] = matchDayOnly;
-           date = new Date(
-             new Date().getFullYear(),
-             new Date().getMonth(),
-             parseInt(day),
-           );
-         }
-       }
-     }
-   }
+  const matchFullDate = dateString.match(regexFullDate);
+  if (matchFullDate) {
+    const [, day, month, year] = matchFullDate;
+    date = new Date(`${year}-${month}-${day}`);
+  } else {
+    const matchShortYear = dateString.match(regexYearShort);
+    if (matchShortYear) {
+      const [, day, month, year] = matchShortYear;
+      date = new Date(`20${year}-${month}-${day}`);
+    } else {
+      const matchMonthDay = dateString.match(regexMonthDay);
+      if (matchMonthDay) {
+        const [, day, month] = matchMonthDay;
+        date = new Date(
+          new Date().getFullYear(),
+          parseInt(month) - 1,
+          parseInt(day),
+        );
+      } else {
+        const matchDayOnly = dateString.match(regexDayOnly);
+        if (matchDayOnly) {
+          const [, day] = matchDayOnly;
+          date = new Date(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            parseInt(day),
+          );
+        }
+      }
+    }
+  }
 
-   if (date && isNaN(date.getTime())) {
-       return null;
-   }
+  if (date && isNaN(date.getTime())) {
+    return null;
+  }
 
-   return date;
+  return date;
 };
 
 
@@ -69,7 +69,7 @@ interface SearchCondition {
   motivo?: { contains: string; mode: "insensitive" };
   tipodeexame?: { contains: string; mode: "insensitive" };
   profissional?: { nome: { contains: string; mode: "insensitive" } };
- Anotacoes?: { some: { anotacao: { contains: string; mode: "insensitive" } } } | undefined;
+  Anotacoes?: { some: { anotacao: { contains: string; mode: "insensitive" } } } | undefined;
   unidade?: { nome: { contains: string; mode: "insensitive" } };
   // Adicione outras possíveis condições aqui se houver
 }
@@ -101,48 +101,50 @@ const Consultaspage = async ({ searchParams }: ConsultaspageProps) => {
   if (profissionalId) {
     whereClause.profissionalId = profissionalId;
   }
-   if (tipo && Object.values(Consultatype).includes(tipo)) {
-     whereClause.tipo = tipo;
-   }
+  if (tipo && Object.values(Consultatype).includes(tipo)) {
+    whereClause.tipo = tipo;
+  }
 
-   if (search) {
- const parsedDate = parseDate(search);
- if (parsedDate && Object.keys(whereClause).length === 0) {
- whereClause.data = { equals: parsedDate };
- } else if (!parsedDate) {
-    const searchConditions: SearchCondition[] = [
+  if (search) {
+    const parsedDate = parseDate(search);
+    if (parsedDate && Object.keys(whereClause).length === 0) {
+      whereClause.data = { equals: parsedDate };
+    } else if (!parsedDate) {
+      const searchConditions: SearchCondition[] = [
         { motivo: { contains: search, mode: "insensitive" as const } as { contains: string; mode: "insensitive" } },
         { tipodeexame: { contains: search, mode: "insensitive" as const } as { contains: string; mode: "insensitive" } },
         {
           profissional: { nome: { contains: search, mode: "insensitive" as const } } as { nome: { contains: string; mode: "insensitive" } },
         },
- { Anotacoes: { some: { anotacao: { contains: search, mode: "insensitive" as const } } } as { some: { anotacao: { contains: string; mode: "insensitive" } } } | undefined },
- { unidade: { nome: { contains: search, mode: "insensitive" as const } } as { nome: { contains: string; mode: "insensitive" } }, },
- ].filter(Boolean);
-    if (Object.keys(whereClause).length > 0) {
+        { Anotacoes: { some: { anotacao: { contains: search, mode: "insensitive" as const } } } as { some: { anotacao: { contains: string; mode: "insensitive" } } } | undefined },
+        { unidade: { nome: { contains: search, mode: "insensitive" as const } } as { nome: { contains: string; mode: "insensitive" } }, },
+      ].filter(Boolean);
+      if (Object.keys(whereClause).length > 0) {
         whereClause.AND = searchConditions;
-    } else {
+      } else {
         whereClause.OR = searchConditions;
+      }
     }
-}
- }
+  }
 
   const consultas: Consultas[] = await db.consultas.findMany({
     where: { ...whereClause, userId: userId }, // Filter by logged-in user ID
     include: {
       profissional: true,
       unidade: true,
- // Anotacoes: true, // No need to include Anotacoes here to filter on them
-      },
+      // Anotacoes: true, // No need to include Anotacoes here to filter on them
+    },
     orderBy: {
-        data: 'desc',
+      data: 'desc',
     }
-    });
+  });
 
-    const profissionaisList: Profissional[] = await db.profissional.findMany({
-        orderBy: { nome: 'asc' },
-    });
-     const tiposConsultaList: Consultatype[] = Object.values(Consultatype);
+  const profissionaisList: Profissional[] = await db.profissional.findMany({
+    where: { userId: userId }, // <-- Adicionado filtro pelo userId do usuário logado
+    orderBy: { nome: 'asc' },
+  });
+
+  const tiposConsultaList: Consultatype[] = Object.values(Consultatype);
 
 
   return (
@@ -150,23 +152,23 @@ const Consultaspage = async ({ searchParams }: ConsultaspageProps) => {
       <Header />
 
       <ConsultaFilter
-         professionals={profissionaisList}
-         consultationTypes={tiposConsultaList}
+        professionals={profissionaisList} // Agora passará a lista filtrada
+        consultationTypes={tiposConsultaList}
       />
 
       <h2 className="text-xs font-bold uppercase text-gray-400 mt-4">
         {search || profissionalId || tipo ?
-         `Resultados da Busca:`
-         : "Todas as Consultas"}
+          `Resultados da Busca:`
+          : "Todas as Consultas"}
       </h2>
 
-       {(search || profissionalId || tipo) && (
-           <p className="text-sm text-gray-500 mb-4">
-               {search && `Busca por texto: "${search}"`}
-               {profissionalId && ` | Profissional ID: ${profissionalId}`}
-               {tipo && ` | Tipo: ${tipo}`}
-           </p>
-       )}
+      {(search || profissionalId || tipo) && (
+        <p className="text-sm text-gray-500 mb-4">
+          {search && `Busca por texto: "${search}"`}
+          {profissionalId && ` | Profissional ID: ${profissionalId}`}
+          {tipo && ` | Tipo: ${tipo}`}
+        </p>
+      )}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {consultas && consultas.length > 0 ? (
