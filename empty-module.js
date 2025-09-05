c89849aff0eb7810edc3f6a15de6c9f734107d18
kaiquekdsp <kaiquekdsp@gmail.com>
2025-09-05 18:18:38 +0000

    Correções de Build

---
+++ empty-module.js
@@ -0,0 +1,2 @@
+// This file is intentionally left empty.
+// It's used in next.config.mjs to mock a module.
--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, {webpack }) => {
-    
+  webpack: (config, { isServer, webpack }) => {
+    // Rule from original config for pdfreader
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(
-        /test\/data\/05-versions-space\.pdf/, 
+        /test\/data\/05-versions-space\.pdf/,
         (resource) => {
           resource.request = require.resolve('./empty-module.js');
         }
       )
     );
 
+    // Rule from original config
     config.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
       type: 'javascript/auto',
     });
 
+    // Rule from original config for pdfjs-dist
     config.externals.push('pdfjs-dist');
-    
+
+    // New rule to prevent bundling tesseract.js on the server
+    if (isServer) {
+      config.externals.push('tesseract.js');
+    }
+
     return config;
   },
 };
